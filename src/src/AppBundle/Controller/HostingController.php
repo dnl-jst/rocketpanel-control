12d207757157472e6794a78ffc0bbc6a55b25744
Daniel Jost <info@daniel-jost.de>
2017-05-21 07:48:58 +0200

    implement GET /hosting

--- src/src/AppBundle/Controller/HostingController.php
+++ src/src/AppBundle/Controller/HostingController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Hosting;
+use Doctrine\ORM\EntityManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -13,12 +15,33 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class HostingController extends Controller
 {
+
     /**
      * @Route("/", name="hosting_get")
      * @Method({"GET"})
      */
     public function indexAction()
     {
-        return new JsonResponse('hosting');
+    	/** @var EntityManager $em */
+    	$em = $this->getDoctrine()->getManager();
+
+    	/** @var Hosting[] $hostings */
+    	$hostings = $em->getRepository('AppBundle:Hosting')->findAll();
+
+		$response = [
+			'elements' => []
+		];
+
+		foreach ($hostings as $hosting) {
+
+			$response['elements'][] = [
+				'hostname' => $hosting->getHostname(),
+				'image'    => $hosting->getImage()->getImageName(),
+				'created'  => $hosting->getCreated()->format(\DateTime::W3C)
+			];
+		}
+
+        return new JsonResponse($response);
     }
+
 }
