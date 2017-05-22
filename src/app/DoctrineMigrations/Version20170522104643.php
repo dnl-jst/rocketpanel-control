164c36856b3e54d075424efa6efe841714b20724
Daniel Jost <info@daniel-jost.de>
2017-05-22 12:47:05 +0200

    make hostname in HostingAlias unique

---
+++ src/app/DoctrineMigrations/Version20170522104643.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170522104643 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_3292EEA3E551C011 ON hosting_aliases (hostname)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_3292EEA3E551C011 ON hosting_aliases');
+    }
+}
--- src/src/AppBundle/Entity/HostingAlias.php
+++ src/src/AppBundle/Entity/HostingAlias.php
@@ -27,7 +27,7 @@ class HostingAlias
 	private $hosting;
 
 	/**
-	 * @ORM\Column(type="string", length=255)
+	 * @ORM\Column(type="string", length=255, unique=true)
 	 */
 	private $hostname;
 
